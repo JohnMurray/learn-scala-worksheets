6f23270e8c8371f98f0cd5408e5befb50c68eeae
John Murray <me@johnmurray.io>
2013-09-25 14:14:55 -0400

    Added remote-actor example
    Not _really_ remote, but close enought to get the point

---
+++ src/main/scala/akka/Actor_6_3.scala
@@ -0,0 +1,41 @@
+import akka.actor.{Props, ActorSystem, ActorSelection, Actor}
+
+/*
+ * A few things to notice:
+ *   - "remote" actor lookup in Local Worker
+ */
+object Actor_6_3 {
+
+  class LocalWorker extends Actor {
+    val remoteActor = context.actorSelection("akka://my-system/user/remote")
+  //val remoteActor = context.actorSelection("akka.tcp://my-system@host:port/user/remote")
+
+    def receive = {
+      case LocalWorker.Greet => {
+        remoteActor ! "Hello"
+      }
+    }
+  }
+  object LocalWorker {
+    case object Greet
+  }
+
+  class RemoteWorker extends Actor {
+    def receive = {
+      case msg: String => {
+        println("received message %s".format(msg))
+        context.system.shutdown()
+      }
+    }
+  }
+
+  def main(args: Array[String]) = {
+    val system = ActorSystem("my-system")
+
+    val remoteWorker = system.actorOf(Props[RemoteWorker], name="remote")
+    val localWorker = system.actorOf(Props[LocalWorker])
+
+    localWorker ! LocalWorker.Greet
+  }
+
+}
